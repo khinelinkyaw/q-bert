54ff8ee77003d567d18cc0ead0559b8ee3fa85ad
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-03-10 21:12:53 +0100

    Separated InputDevice and InputManager

---
+++ Minigin/InputDevice.cpp
@@ -0,0 +1,120 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "InputDevice.h"
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_keyboard.h>
+#include <SDL3/SDL_scancode.h>
+#include <Xinput.h>
+
+bool dae::InputDevice::IsPressed(InputAction action) const
+{
+    return !GetPreviousKeyState(action) && GetCurrentKeyState(action);
+}
+
+bool dae::InputDevice::IsReleased(InputAction action) const
+{
+    return GetPreviousKeyState(action) && !GetCurrentKeyState(action);
+}
+
+bool dae::InputDevice::IsHeld(InputAction action) const
+{
+    return GetPreviousKeyState(action) && GetCurrentKeyState(action);
+}
+
+bool dae::InputDevice::IsDown(InputAction action) const
+{
+    return GetCurrentKeyState(action);
+}
+
+bool dae::InputDevice::IsUp(InputAction action) const
+{
+    return !GetCurrentKeyState(action);
+}
+
+void dae::KeyboardInputDevice::UpdateState()
+{
+    int l_NextStateIndex{ 1 - m_CurrentStateIndex };
+
+    SDL_PumpEvents();
+
+    for (int i = 0; i < m_NumKeys; ++i)
+    {
+        m_KeyStates[l_NextStateIndex][i] = m_KeyStatesPtr[i];
+    }
+
+    m_CurrentStateIndex = l_NextStateIndex;
+}
+
+bool dae::KeyboardInputDevice::GetPreviousKeyState(InputAction action) const
+{
+    int l_PreviousStateIndex = 1 - m_CurrentStateIndex;
+    int scancode = m_Keymap.at(action);
+
+    return m_KeyStates[l_PreviousStateIndex][scancode];
+}
+
+bool dae::KeyboardInputDevice::GetCurrentKeyState(InputAction action) const
+{
+    int scancode = m_Keymap.at(action);
+
+    return m_KeyStates[m_CurrentStateIndex][scancode];
+}
+
+dae::KeyboardInputDevice::KeyboardInputDevice()
+    : m_NumKeys{}
+    , m_KeyStates{}
+    , m_KeyStatesPtr(SDL_GetKeyboardState(&m_NumKeys))
+
+{
+    // TODO: make this configurable
+    m_Keymap = {
+        { InputAction::MoveUp, SDL_SCANCODE_W },
+        { InputAction::MoveLeft, SDL_SCANCODE_A },
+        { InputAction::MoveDown, SDL_SCANCODE_S },
+        { InputAction::MoveRight, SDL_SCANCODE_D }
+    };
+
+    m_KeyStates[0].resize(m_NumKeys);
+    m_KeyStates[1].resize(m_NumKeys);
+}
+
+void dae::GamepadInputDevice::UpdateState()
+{
+    int l_NextStateIndex{ 1 - m_CurrentStateIndex };
+
+    ZeroMemory(&m_KeyStates[l_NextStateIndex], sizeof(XINPUT_STATE));
+    XInputGetState(m_ControllerIndex, &m_KeyStates[l_NextStateIndex]);
+
+    m_CurrentStateIndex = l_NextStateIndex;
+}
+
+dae::GamepadInputDevice::GamepadInputDevice(int controllerIndex)
+    : m_ControllerIndex(controllerIndex)
+    , m_KeyStates{}
+{
+    // TODO: make this default parameter (better API visibility)
+    // TODO: make this configurable
+    // လုပ်စရာတွေ အများကြီးရှိသေးတယ်။
+    m_Keymap = {
+        { InputAction::MoveUp, XINPUT_GAMEPAD_DPAD_UP },
+        { InputAction::MoveLeft, XINPUT_GAMEPAD_DPAD_LEFT },
+        { InputAction::MoveDown, XINPUT_GAMEPAD_DPAD_DOWN },
+        { InputAction::MoveRight, XINPUT_GAMEPAD_DPAD_RIGHT }
+    };
+}
+
+bool dae::GamepadInputDevice::GetPreviousKeyState(InputAction action) const
+{
+    int l_PreviousStateIndex = 1 - m_CurrentStateIndex;
+    int button = m_Keymap.at(action);
+
+    return m_KeyStates[l_PreviousStateIndex].Gamepad.wButtons & button;
+}
+
+bool dae::GamepadInputDevice::GetCurrentKeyState(InputAction action) const
+{
+    int button = m_Keymap.at(action);
+
+    return m_KeyStates[m_CurrentStateIndex].Gamepad.wButtons & button;
+}
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,93 +1,13 @@
 #ifndef INPUT_MANAGER_H
 #define INPUT_MANAGER_H
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-#include "Components/ControllerComponent.h"
+#include "InputDevice.h"
 #include "Singleton.h"
-#include <Xinput.h>
-#include <array>
-#include <memory>
-#include <unordered_map>
 #include <vector>
 
 namespace dae
 {
-    enum class InputAction
-    {
-        MoveUp,
-        MoveLeft,
-        MoveDown,
-        MoveRight,
-    };
-
-    enum class InputDeviceType
-    {
-        Keyboard,
-        Gamepad
-    };
-
-    class InputDevice
-    {
-    protected:
-        int m_CurrentStateIndex{ 0 };
-        std::unordered_map<InputAction, int> m_Keymap{};
-
-        virtual bool GetPreviousKeyState(InputAction action) const = 0;
-        virtual bool GetCurrentKeyState(InputAction action) const = 0;
-    public:
-        bool IsPressed(InputAction action) const;
-        bool IsReleased(InputAction action) const;
-        bool IsHeld(InputAction action) const;
-        bool IsDown(InputAction action) const;
-        bool IsUp(InputAction action) const;
-
-        virtual void UpdateState() = 0;
-
-        InputDevice() = default;
-        virtual ~InputDevice() = default;
-    };
-
-    class KeyboardInputDevice final : public InputDevice
-    {
-    private:
-        int m_NumKeys;
-        std::array<std::vector<bool>, 2> m_KeyStates;
-        bool const* m_KeyStatesPtr;
-
-        bool GetPreviousKeyState(InputAction action) const override;
-        bool GetCurrentKeyState(InputAction action) const override;
-    public:
-        void UpdateState() override;
-
-        KeyboardInputDevice();
-        ~KeyboardInputDevice() override = default;
-    };
-
-    class GamepadInputDevice final : public InputDevice
-    {
-    private:
-        DWORD m_ControllerIndex;
-        std::array<XINPUT_STATE, 2> m_KeyStates;
-
-        bool GetPreviousKeyState(InputAction action) const override;
-        bool GetCurrentKeyState(InputAction action) const override;
-
-    public:
-        void UpdateState() override;
-
-        GamepadInputDevice(int controllerIndex);
-        ~GamepadInputDevice() override = default;
-    };
-
-    struct ControllerInfo final
-    {
-        ControllerComponent* m_PlayerController{ nullptr };
-        InputDeviceType m_InputType{ InputDeviceType::Keyboard };
-        std::unique_ptr<InputDevice> m_InputDevice{ nullptr };
-    };
-
+    class ControllerComponent;
     class InputManager final : public Singleton<InputManager>
     {
     private:
