600611b8b80f17e118281b56667a088b395b9ab0
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-04-12 18:34:58 +0200

    Engine: Observer class now bind events

--- Engine/Decoupling/Observer.h
+++ Engine/Decoupling/Observer.h
@@ -1,23 +1,41 @@
-#include <string>
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
+#include <string>
+#include <unordered_map>
+
 namespace GameEngine
 {
+    class TextComponent;
     class GameObject;
+
     class Observer
     {
+    private:
+        std::unordered_map<std::string, void(*)(GameObject&)> m_EventBindings;
+
     public:
-        virtual void OnNotify(GameObject const& gameObject, std::string eventId) = 0;
+        void OnNotify(GameObject& gameObject, std::string eventId);
+        void BindEvent(std::string eventId, void(*func)(GameObject&));
+
+        Observer();
         virtual ~Observer() = default;
     };
 
-    class NullObserver final : public Observer
-    {
-    public:
-        void OnNotify(GameObject const&, std::string) override {};
-        ~NullObserver() override = default;
-    };
+    //class NullObserver final : public Observer
+    //{
+    //public:
+    //    void OnNotify(GameObject const&, std::string) override {};
+    //    ~NullObserver() override = default;
+    //};
+
+    //class TextObserver : public Observer
+    //{
+    //private:
+    //    TextComponent* m_pTextComponent;
+    //public:
+    //    ~TextObserver() override = default;
+    //};
 }
 
 #endif
