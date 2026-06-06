fc47040711c55b3b1298212665aac7b8f1e002ad
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-06 11:00:41 +0200

    [Game] Constants: Sprite maps for Ugg and Wrongway

--- Game/Source/Misc/Constants.h
+++ Game/Source/Misc/Constants.h
@@ -64,10 +64,24 @@ namespace Game
             {static_cast<int>(Direction::DownLeft) + static_cast<int>(MovementEvent::OnHop),     7 },
         };
 
+        std::unordered_map<int, int> const UGG_SPRITE_MAP{
+            {static_cast<int>(Direction::UpLeft) + static_cast<int>(MovementEvent::OnIdle),      0 },
+            {static_cast<int>(Direction::UpLeft) + static_cast<int>(MovementEvent::OnHop),       1 },
+            {static_cast<int>(Direction::DownLeft) + static_cast<int>(MovementEvent::OnIdle),    4 },
+            {static_cast<int>(Direction::DownLeft) + static_cast<int>(MovementEvent::OnHop),     5 },
+        };
+
+        std::unordered_map<int, int> const WRONGWAY_SPRITE_MAP{
+            {static_cast<int>(Direction::DownRight) + static_cast<int>(MovementEvent::OnIdle),   0 },
+            {static_cast<int>(Direction::DownRight) + static_cast<int>(MovementEvent::OnHop),    1 },
+            {static_cast<int>(Direction::UpRight) + static_cast<int>(MovementEvent::OnIdle),     4 },
+            {static_cast<int>(Direction::UpRight) + static_cast<int>(MovementEvent::OnHop),      5 },
+        };
+
         namespace Enemy
         {
             float constexpr IDLE_DURATION{ 1.f };
-            int constexpr   MAX_RED_GREEN_SLIME_PATH_SIZE{ 6 };
+            int constexpr   MAX_NORMAL_ENEMY_PATH_SIZE{ 6 };
             int constexpr   MAX_PURPLE_SLIME_PATH_SIZE{ 5 };
         };
     }
