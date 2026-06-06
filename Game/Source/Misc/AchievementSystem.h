3ce429d797df6b8f0bf9f6b77788c0312129f705
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-06 10:50:44 +0200

    [Game] AchievementSystem: vec3 to vec2

--- Game/Source/Misc/AchievementSystem.h
+++ Game/Source/Misc/AchievementSystem.h
@@ -27,7 +27,7 @@ namespace Game
     public:
         void FixedUpdate() override {};
         void Update() override {};
-        void Render(vec3 const&) const override {};
+        void Render(vec2 const&) const override {};
 
         AchievementSystem* GetAchievementSystem() { return &m_AchievementSystem; };
 
