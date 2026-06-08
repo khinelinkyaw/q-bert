95819369a1e53423b9a6cc3fb3b0506ecfa6af81
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-08 11:36:05 +0200

    [Game] CreatureSpawner can now spawn creatures! Building components for creatures moved to CreatureFactory

--- Game/Source/Components/CreatureSpawner.cpp
+++ Game/Source/Components/CreatureSpawner.cpp
@@ -1,163 +1,59 @@
 #include "CreatureSpawner.h"
 
-#include <Engine/Animation/Animation.h>
-#include <Engine/Components/CollisionComponent.h>
-#include <Engine/Components/TextureComponent.h>
-#include <Engine/Core/GameObject.h>
-#include <Engine/Core/SceneManager.h>
-#include <Engine/Misc/Enums.h>
 #include <Engine/Components/BaseComponent.h>
+#include <Engine/Core/GameObject.h>
+#include <Engine/Core/Random.h>
+#include <Engine/Core/SceneManager.h>
+#include <Engine/Core/Time.h>
 
 #include <Components/BaseCreature.h>
-#include <Components/Controllers/ControllerComponent.h>
-#include <Components/Controllers/CoilyController.h>
-#include <Components/Controllers/GenericEnemyController.h>
+#include <Creatures/CreatureFactory.h>
 #include <Map/Graph.h>
 
-GameEngine::GameObject& Game::CreatureSpawner::ConstructCoily(int graphBlockId)
+void Game::CreatureSpawner::Update()
 {
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    object.GetTransform()->SetLocalPosition(m_pGraph->GetBlockSurfaceCenter(graphBlockId, BlockSurface::Top));
-    object.AddComponent<GameEngine::SpriteComponent>()->Init("Coily.png", 1, 8);
-    object.AddComponent<BaseCreature>()->Init(Creature::Coily);
-    object.AddComponent<CoilyController>();
-    object.AddComponent<GameEngine::CollisionComponent>();
-    object.GetComponent<GameEngine::TextureComponent>()->SetPivot(GameEngine::Pivot::MiddleDown);
-    return object;
+    m_ElapsedTime += GameEngine::GetDeltaTime();
+
+    if (m_ElapsedTime >= m_SpawnInterval)
+    {
+        m_ElapsedTime -= m_SpawnInterval;
+
+        auto& rng{ GameEngine::Random::GetRNG() };
+        int spawnBlockId{ m_SpawnBlockIds[rng() % m_SpawnBlockIds.size()] };
+        auto spawnPosition{ m_pGraph->GetBlockSurfaceCenter(spawnBlockId, m_SpawnSurface) };
+
+        auto& creature{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject()};
+        CreatureFactory::BuildCreatureComponents(creature, m_CreatureType);
+        creature.GetTransform()->SetLocalPosition(spawnPosition);
+    }
 }
 
-GameEngine::GameObject& Game::CreatureSpawner::ConstructPurpleSlime(int graphBlockId)
+void Game::CreatureSpawner::Init(Creature creatureType, float spawnInterval)
 {
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    object.GetTransform()->SetLocalPosition(m_pGraph->GetBlockSurfaceCenter(graphBlockId, BlockSurface::Top));
-    object.AddComponent<GameEngine::SpriteComponent>()->Init("Slimes.png", 1, 6);
-    object.AddComponent<BaseCreature>()->Init(Creature::PurpleSlime);
-    object.AddComponent<GenericEnemyController>()->Init(Creature::PurpleSlime);
-    object.AddComponent<GameEngine::CollisionComponent>();
-    object.GetComponent<GameEngine::TextureComponent>()->SetPivot(GameEngine::Pivot::MiddleDown);
-    return object;
-}
+    m_CreatureType = creatureType;
+    m_SpawnInterval = spawnInterval;
 
-GameEngine::GameObject& Game::CreatureSpawner::ConstructGreenSlime(int graphBlockId)
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    object.GetTransform()->SetLocalPosition(m_pGraph->GetBlockSurfaceCenter(graphBlockId, BlockSurface::Top));
-    object.AddComponent<GameEngine::SpriteComponent>()->Init("Slimes.png", 1, 6);
-    object.AddComponent<BaseCreature>()->Init(Creature::GreenSlime);
-    object.AddComponent<GenericEnemyController>()->Init(Creature::GreenSlime);
-    object.AddComponent<GameEngine::CollisionComponent>();
-    object.GetComponent<GameEngine::TextureComponent>()->SetPivot(GameEngine::Pivot::MiddleDown);
-    return object;
-}
+    switch (creatureType)
+    {
+    case Creature::Ugg:
+        m_SpawnBlockIds = RIGHT_SPAWN_POINTS;
+        break;
+    case Creature::WrongWay:
+        m_SpawnBlockIds = LEFT_SPAWN_POINTS;
+        break;
+    default:
+        m_SpawnBlockIds = TOP_SPAWN_POINTS;
+        break;
+    }
 
-GameEngine::GameObject& Game::CreatureSpawner::ConstructRedSlime(int graphBlockId)
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    object.GetTransform()->SetLocalPosition(m_pGraph->GetBlockSurfaceCenter(graphBlockId, BlockSurface::Top));
-    object.AddComponent<GameEngine::SpriteComponent>()->Init("Slimes.png", 1, 6);
-    object.AddComponent<BaseCreature>()->Init(Creature::RedSlime);
-    object.AddComponent<GenericEnemyController>()->Init(Creature::RedSlime);
-    object.AddComponent<GameEngine::CollisionComponent>();
-    object.GetComponent<GameEngine::TextureComponent>()->SetPivot(GameEngine::Pivot::MiddleDown);
-    return object;
+    if (auto iter{ CreatureFactory::CREATURE_TABLE.find(creatureType) }; iter != CreatureFactory::CREATURE_TABLE.end())
+    {
+        m_SpawnSurface = iter->second.SpawnSurface;
+    }
 }
 
 Game::CreatureSpawner::CreatureSpawner(GameEngine::GameObject* owner)
     : BaseComponent{ owner }
     , m_pGraph{ GameEngine::SceneManager::Get().GetObjectByName("Graph")->GetComponent<Graph>() }
 {
-}
-
-void Game::Builder::AddCommonComponents(GameEngine::GameObject& gameObject, GameEngine::Pivot pivot)
-{
-    gameObject.AddComponent<GameEngine::CollisionComponent>();
-    gameObject.AddComponent<GameEngine::SpriteComponent>();
-    gameObject.AddComponent<BaseCreature>();
-    gameObject.AddComponent<GameEngine::TextureComponent>()->SetPivot(pivot);
-}
-
-void Game::Builder::InitQBertComponents(GameEngine::GameObject& gameObject)
-{
-    gameObject.AddComponent<ControllerComponent>();
-
-    gameObject.GetComponent<GameEngine::SpriteComponent>()->Init("Qbert.png", 1, 8);
-    gameObject.GetComponent<BaseCreature>()->Init(Creature::QBert);
-}
-
-void Game::Builder::InitSlimeComponents(GameEngine::GameObject& gameObject, Creature creatureType)
-{
-    gameObject.AddComponent<GenericEnemyController>()->Init(creatureType);
-
-    gameObject.GetComponent<GameEngine::SpriteComponent>()->Init("Slimes.png", 1, 6);
-    gameObject.GetComponent<BaseCreature>()->Init(creatureType);
-}
-
-void Game::Builder::InitCoilyComponents(GameEngine::GameObject& gameObject)
-{
-    gameObject.AddComponent<CoilyController>();
-
-    gameObject.GetComponent<GameEngine::SpriteComponent>()->Init("Coily.png", 1, 8);
-    gameObject.GetComponent<BaseCreature>()->Init(Creature::Coily);
-}
-
-GameEngine::GameObject& Game::Builder::BuildQBert()
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    AddCommonComponents(object, GameEngine::Pivot::MiddleDown);
-    InitQBertComponents(object);
-    return object;
-}
-
-GameEngine::GameObject& Game::Builder::BuildCoily()
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    AddCommonComponents(object);
-    InitCoilyComponents(object);
-    return object;
-}
-
-GameEngine::GameObject& Game::Builder::BuildPurpleSlime()
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    AddCommonComponents(object);
-    InitSlimeComponents(object, Creature::PurpleSlime);
-    return object;
-}
-
-GameEngine::GameObject& Game::Builder::BuildGreenSlime()
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    AddCommonComponents(object);
-    InitSlimeComponents(object, Creature::GreenSlime);
-    return object;
-}
-
-GameEngine::GameObject& Game::Builder::BuildRedSlime()
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    AddCommonComponents(object);
-    InitSlimeComponents(object, Creature::RedSlime);
-    return object;
-}
-
-GameEngine::GameObject& Game::Builder::BuildUgg()
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    AddCommonComponents(object, GameEngine::Pivot::LeftUp);
-    object.GetComponent<GameEngine::TextureComponent>()->SetOriginOffset({2.f, 2.f});
-    object.AddComponent<GenericEnemyController>()->Init(Creature::Ugg);
-    object.GetComponent<GameEngine::SpriteComponent>()->Init("Ugg.png", 1, 8);
-    object.GetComponent<BaseCreature>()->Init(Creature::Ugg);
-    return object;
-}
-
-GameEngine::GameObject& Game::Builder::BuildWrongWay()
-{
-    GameEngine::GameObject& object{ GameEngine::SceneManager::Get().GetActiveScene()->CreateGameObject() };
-    AddCommonComponents(object, GameEngine::Pivot::RightUp);
-    object.GetComponent<GameEngine::TextureComponent>()->SetOriginOffset({ -2.f, 2.f });
-    object.AddComponent<GenericEnemyController>()->Init(Creature::WrongWay);
-    object.GetComponent<GameEngine::SpriteComponent>()->Init("Wrongway.png", 1, 8);
-    object.GetComponent<BaseCreature>()->Init(Creature::WrongWay);
-    return object;
 }
--- Game/Source/Components/CreatureSpawner.h
+++ Game/Source/Components/CreatureSpawner.h
@@ -5,44 +5,34 @@
 
 #include <Engine/Components/BaseComponent.h>
 #include <Engine/Core/GameObject.h>
-#include <Engine/Misc/Enums.h>
+#include <Engine/Misc/Types.h>
 
+#include <Misc/Enums.h>
 #include <vector>
 
 namespace Game
 {
-    namespace Builder
-    {
-        void AddCommonComponents(GameEngine::GameObject& gameObject, GameEngine::Pivot pivot = GameEngine::Pivot::MiddleDown);
-        void InitQBertComponents(GameEngine::GameObject& gameObject);
-        void InitCoilyComponents(GameEngine::GameObject& gameObject);
-        void InitSlimeComponents(GameEngine::GameObject& gameObject, Creature creatureType);
-
-        GameEngine::GameObject& BuildQBert();
-        GameEngine::GameObject& BuildCoily();
-        GameEngine::GameObject& BuildPurpleSlime();
-        GameEngine::GameObject& BuildGreenSlime();
-        GameEngine::GameObject& BuildRedSlime();
-        GameEngine::GameObject& BuildUgg();
-        GameEngine::GameObject& BuildWrongWay();
-    }
-
-
     class Graph;
     class CreatureSpawner final : public GameEngine::BaseComponent
     {
     private:
+        static inline std::vector<int> const TOP_SPAWN_POINTS{ 1, 2 };
+        static inline std::vector<int> const LEFT_SPAWN_POINTS{ 21 };
+        static inline std::vector<int> const RIGHT_SPAWN_POINTS{ 27 };
+
         Graph* m_pGraph{};
+        Creature m_CreatureType{};
+        BlockSurface m_SpawnSurface{};
         float m_ElapsedTime{};
         float m_SpawnInterval{ 5.f };
-        std::vector<int> m_SpawnBlockIds{};
+        std::vector<int> m_SpawnBlockIds{ TOP_SPAWN_POINTS };
 
     public:
-        GameEngine::GameObject& ConstructCoily(int graphBlockId);
-        GameEngine::GameObject& ConstructPurpleSlime(int graphBlockId);
-        GameEngine::GameObject& ConstructGreenSlime(int graphBlockId);
-        GameEngine::GameObject& ConstructRedSlime(int graphBlockId);
+        void FixedUpdate() override {};
+        void Update() override;
+        void Render(vec2 const&) const override {};
 
+        void Init(Creature creatureType, float spawnInterval = 5.f);
         CreatureSpawner(GameEngine::GameObject* owner);
         ~CreatureSpawner() override = default;
     };
