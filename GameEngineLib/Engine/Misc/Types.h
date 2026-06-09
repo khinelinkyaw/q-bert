ea2092024800a05be545f1f84567f55b6c860a01
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-09 23:42:15 +0200

    [GameEngine] Rectf shorthand for Rect<float>

--- GameEngineLib/Engine/Misc/Types.h
+++ GameEngineLib/Engine/Misc/Types.h
@@ -1,11 +1,14 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <Engine/Misc/Structs.h>
+
 #include <glm/glm.hpp>
 #include <glm/fwd.hpp>
 
 using ObjectID = int;
 using vec2 = glm::vec2;
 using vec3 = glm::vec3;
+using Rectf = GameEngine::Rect<float>;
 
 #endif
