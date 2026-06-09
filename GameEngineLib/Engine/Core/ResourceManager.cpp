8040ee2c09af05d9fe14a7f6fc9e995689483ac4
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-09 23:43:57 +0200

    [GameEngine] ResourceManager: Exception handling for when a Texture2D can't be created

--- GameEngineLib/Engine/Core/ResourceManager.cpp
+++ GameEngineLib/Engine/Core/ResourceManager.cpp
@@ -29,7 +29,17 @@ std::shared_ptr<Texture2D> ResourceManager::LoadTexture(const std::string& file)
     const auto fullPath = m_dataPath / file;
     const auto filename = fs::path(fullPath).filename().string();
     if (m_loadedTextures.find(filename) == m_loadedTextures.end())
-        m_loadedTextures.insert(std::pair(filename, std::make_shared<Texture2D>(fullPath.string())));
+    {
+        try
+        {
+            m_loadedTextures.insert(std::pair(filename, std::make_shared<Texture2D>(fullPath.string())));
+        }
+        catch (std::runtime_error const&)
+        {
+            return nullptr;
+        }
+    }
+
     return m_loadedTextures.at(filename);
 }
 
