ce7de57256979bbee0effe4df901937e7c76356c
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-09 23:10:44 +0200

    [GameEngine] UI Utils functions inlined

--- GameEngineLib/Engine/UI/Utils.h
+++ GameEngineLib/Engine/UI/Utils.h
@@ -3,69 +3,73 @@
 
 #include <glm/glm.hpp>
 
-#include <Engine/Misc/Structs.h>
 #include <Engine/Misc/Constants.h>
 #include <Engine/Misc/Enums.h>
+#include <Engine/Misc/Structs.h>
+#include <Engine/Misc/Types.h>
 
-namespace GameEngine::UI
+namespace GameEngine
 {
-    glm::vec2 AlignToWindow(Pivot pivot, float hMargin, float vMargin)
+    namespace UI
     {
-        auto [hDir, vDir] { Constants::UI_PIVOT_MAP.at(pivot)};
+        inline vec2 AlignToWindow(Pivot pivot, float hMargin, float vMargin)
+        {
+            auto [hDir, vDir] { Constants::UI_PIVOT_MAP.at(pivot) };
 
-        float hAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(hDir) };
-        float vAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(vDir) };
+            float hAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(hDir) };
+            float vAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(vDir) };
 
-        float xPos{ static_cast<float>(Constants::WINDOW_WIDTH) * hAlighment };
-        float yPos{ static_cast<float>(Constants::WINDOW_HEIGHT) * vAlighment };
+            float xPos{ static_cast<float>(Constants::WINDOW_WIDTH) * hAlighment };
+            float yPos{ static_cast<float>(Constants::WINDOW_HEIGHT) * vAlighment };
 
-        //float xSign{ Constants::UI_DIRECTION_MULTI_MAP.at(hDir) };
-        //float ySign{ Constants::UI_DIRECTION_MULTI_MAP.at(vDir) };
+            //float xSign{ Constants::UI_DIRECTION_MULTI_MAP.at(hDir) };
+            //float ySign{ Constants::UI_DIRECTION_MULTI_MAP.at(vDir) };
 
-        xPos += hMargin;
-        yPos += vMargin;
+            xPos += hMargin;
+            yPos += vMargin;
 
-        return glm::vec2{ xPos, yPos};
-    }
+            return { xPos, yPos };
+        }
 
-    glm::vec2 AlignToWindow(float hPercent, float vPercent)
-    {
-        float xPos{ Constants::WINDOW_WIDTH * hPercent };
-        float yPos{ Constants::WINDOW_HEIGHT * vPercent };
+        inline vec2 AlignToWindow(float hPercent, float vPercent)
+        {
+            float xPos{ Constants::WINDOW_WIDTH * hPercent };
+            float yPos{ Constants::WINDOW_HEIGHT * vPercent };
 
-        return glm::vec2{ xPos, yPos };
-    }
+            return { xPos, yPos };
+        }
 
-    glm::vec2 AlignToRect(float hOffset, float vOffset, Rect<float> rect, Pivot pivot = Pivot::LeftUp)
-    {
-        auto [hDir, vDir] { Constants::UI_PIVOT_MAP.at(pivot) };
+        inline vec2 AlignToRect(float hOffset, float vOffset, Rect<float> rect, Pivot pivot = Pivot::LeftUp)
+        {
+            auto [hDir, vDir] { Constants::UI_PIVOT_MAP.at(pivot) };
 
-        float hAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(hDir) };
-        float vAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(vDir) };
+            float hAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(hDir) };
+            float vAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(vDir) };
 
-        float xPos{ (static_cast<float>(rect.width) * hAlighment) + rect.x };
-        float yPos{ (static_cast<float>(rect.height) * vAlighment) + rect.y };
+            float xPos{ (static_cast<float>(rect.width) * hAlighment) + rect.x };
+            float yPos{ (static_cast<float>(rect.height) * vAlighment) + rect.y };
 
-        xPos += hOffset;
-        yPos += vOffset;
+            xPos += hOffset;
+            yPos += vOffset;
 
-        return glm::vec2{ xPos, yPos };
-    }
+            return { xPos, yPos };
+        }
 
-    glm::vec2 AlignToRect(glm::vec2 offset, Rect<float> rect, Pivot pivot = Pivot::LeftUp)
-    {
-        auto [hDir, vDir] { Constants::UI_PIVOT_MAP.at(pivot) };
+        inline vec2 AlignToRect(vec2 offset, Rect<float> rect, Pivot pivot = Pivot::LeftUp)
+        {
+            auto [hDir, vDir] { Constants::UI_PIVOT_MAP.at(pivot) };
 
-        float hAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(hDir) };
-        float vAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(vDir) };
+            float hAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(hDir) };
+            float vAlighment{ Constants::UI_DIRECTION_OFFSET_MAP.at(vDir) };
 
-        float xPos{ (static_cast<float>(rect.width) * hAlighment) + rect.x };
-        float yPos{ (static_cast<float>(rect.height) * vAlighment) + rect.y };
+            float xPos{ (static_cast<float>(rect.width) * hAlighment) + rect.x };
+            float yPos{ (static_cast<float>(rect.height) * vAlighment) + rect.y };
 
-        xPos += offset.x;
-        yPos += offset.y;
+            xPos += offset.x;
+            yPos += offset.y;
 
-        return glm::vec2{ xPos, yPos };
+            return { xPos, yPos };
+        }
     }
 }
 
