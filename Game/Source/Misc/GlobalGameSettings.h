0aa4e7a48e4b49da73779737d3f600924391a087
Khine LIN KYAW <khine.lin.kyaw@student.howest.be>
2026-06-12 16:48:16 +0200

    [Game] GlobalGameSettings for SelectedGameMode to be visible between different scenes

---
+++ Game/Source/Misc/GlobalGameSettings.h
@@ -0,0 +1,14 @@
+#ifndef GLOBAL_GAME_SETTINGS_H
+#define GLOBAL_GAME_SETTINGS_H
+
+#include <Misc/Enums.h>
+
+namespace Game
+{
+    namespace GlobalGameSettings
+    {
+        inline Gamemode SelectedGamemode{ Gamemode::Solo };
+    }
+}
+
+#endif
